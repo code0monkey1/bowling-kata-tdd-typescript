808823758122d39a47f38b11f1f005002086073b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-23 07:47:12 +0000

    23 May 2023, 13:17

--- src/Game.ts
+++ src/Game.ts
@@ -1,3 +1,5 @@
+
+
 /**
  * 
  */
@@ -15,14 +17,19 @@ class Game{
    
    let CHANCE_INDEX=0
 
-   const SPARE=10
-
     for( let frame=0;frame<FRAMES;frame++){
        
-        if(this._rolls[CHANCE_INDEX]+this._rolls[CHANCE_INDEX+1]===SPARE){
+        if(this.isSpare(CHANCE_INDEX,CHANCE_INDEX+1)){
+           
+            this._score= 10+this._rolls[CHANCE_INDEX+2]
+            CHANCE_INDEX+=2
 
-          
+          }
+        else{
 
+           this._score+= this._rolls[CHANCE_INDEX]+
+                         this._rolls[CHANCE_INDEX+1]
+           CHANCE_INDEX+=2
         }
          
     }
@@ -32,11 +39,17 @@ class Game{
      
   }
 
-  public roll(pins:number){
+    public roll(pins:number){
 
-   this._rolls[this.currentRollIndex++]=pins
+    this._rolls[this.currentRollIndex++]=pins
 
-  }
+    }
+
+
+     public  isSpare=(score_1:number,score_2:number):Boolean=>{
+
+      return score_1+score_2 ===10
+    }
 
 }
 
--- tests/GameTestHelper.ts
+++ tests/GameTestHelper.ts
@@ -7,8 +7,3 @@ export const rollMany=(game:Game,rolls:number,pins:number)=>{
   }
 
 }
-
-export const isSpare=(score_1:number,score_2:number):Boolean=>{
-
-  return score_1+score_2 ===10
-}
